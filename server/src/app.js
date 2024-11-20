c0759744ac5cde87a1261fb2d501ab794cfdbc5c
abhishek <shukla13122000@gmail.com>
2024-11-20 21:03:08 +0530

    add database connection

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" })); // declaring that you will be getting json upto limit of 16kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // urlencoded for accepting different types of params
+app.use(express.static("public"));
+
+export { app };
---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB().then(() => {
+  app.on("error", (error) => {
+    console.log(error);
+    throw error;
+  });
+
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`App is listeining on port ${process.env.PORT}`);
+  });
+});
