4da9536b1e370cf5c5f5885c11ae7a01052f2600
abhishek <shukla13122000@gmail.com>
2024-11-21 16:52:53 +0530

    add apiservice using axios and add register/login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,10 +2,57 @@ import { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import { motion } from "framer-motion";
+import { postData } from "../../apiService";
+import { toast } from "react-toastify";
 const Login = () => {
   const [state, setState] = useState("Login");
-  const { setShowLogin } = useContext(AppContext);
+  const { setShowLogin, setUser, setToken } = useContext(AppContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (state == "Login") {
+        let res = await postData("/api/v1/users/login", {
+          email,
+          password,
+        });
+        if (res.success) {
+          setToken(res?.data?.token);
+          localStorage.setItem("token", res?.data?.token);
+          setUser(res.data);
+          setShowLogin(false);
+          setName("");
+          setEmail("");
+          setPassword("");
+        } else {
+          toast.error("Error occured while login");
+        }
+      } else {
+        let res = await postData("/api/v1/users/register", {
+          name,
+          email,
+          password,
+        });
+        if (res.success) {
+          setToken(res?.data?.token);
+          localStorage.setItem("token", res?.data?.token);
+          setUser(res.data);
+          setShowLogin(false);
+          setName("");
+          setEmail("");
+          setPassword("");
+        } else {
+          toast.error("Error occured while registering");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
   // remove scroll functionality when login form is mounted
 
   useEffect(() => {
@@ -16,15 +63,13 @@ const Login = () => {
   }, []);
 
   return (
-    <div
-      onClick={() => setShowLogin(false)}
-      className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center"
-    >
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
       <motion.form
         initial={{ opacity: 0.2, y: 100 }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
+        onSubmit={handleSubmit}
         className="relative bg-white p-10 rounded-xl text-gray-500"
       >
         <h1 className="text-2xl md:text-3xl lg:text-4xl mb-4 ml-1 font-semibold">
@@ -35,6 +80,8 @@ const Login = () => {
             <div className="flex gap-2 items-center   border  px-6 py-2  rounded-full">
               <img src={assets.profile_icon} className="w-6" alt="" />
               <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 className="outline-none"
                 type="text"
                 placeholder="Full Name"
@@ -47,8 +94,10 @@ const Login = () => {
             <img src={assets.email_icon} className="w-4" alt="" />
             <input
               className="outline-none text-sm"
-              type="text"
+              type="email"
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
@@ -56,7 +105,9 @@ const Login = () => {
             <img src={assets.lock_icon} className="w-4" alt="" />
             <input
               className="outline-none"
-              type="text"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               required
             />
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,12 +6,19 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(false);
-
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [credit, setCredit] = useState(0);
   const value = {
     user,
     setUser,
     showLogin,
     setShowLogin,
+    BACKEND_URL,
+    token,
+    setToken,
+    credit,
+    setCredit,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
